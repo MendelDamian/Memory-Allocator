d686638ccbd1b4e38a9aec26ded586af53e92be1
Damian Mendel <mendeldamian01@gmail.com>
2022-07-27 13:35:12 +0200

    WIP malloc

---
+++ heap.c
@@ -0,0 +1,142 @@
+#include "heap.h"
+#include "tested_declarations.h"
+#include "rdebug.h"
+
+MEMORY_MANAGER memory_manager;
+
+#define PAGE_SIZE 4096
+#define FENCE 0xEA
+#define FENCES 16
+#define SBRK_FAIL ((void *)-1)
+
+#define ALIGN(n) (((n) + 7) & (-8))
+
+#define ALIGN_PAGE(n) (((n) + (PAGE_SIZE - 1)) & (-PAGE_SIZE))
+
+#define MEMORY_CHUNK_DATA_ADDRESS(chunk) ((char *)(chunk) + sizeof(MEMORY_CHUNK) + FENCES)
+
+#define MEMORY_CHUNK_OFFSET(chunk) ((char *)(chunk) - (char *)memory_manager.memory_start)
+
+#define MEMORY_CHUNK_PHYSICAL_SIZE(chunk)                                               \
+    ((chunk)->next                                                                      \
+    ? ((size_t)(chunk)->next - (size_t)(chunk))                                         \
+    : (sizeof(MEMORY_CHUNK) + FENCES + (chunk)->size + FENCES))
+
+#define MEMORY_CHUNK_DATA_PHYSICAL_SIZE(chunk) \
+    (MEMORY_CHUNK_PHYSICAL_SIZE(chunk) - sizeof(MEMORY_CHUNK))
+
+#define MEMORY_CHUNK_NEXT(chunk) ((MEMORY_CHUNK *)((char *)(chunk) + MEMORY_CHUNK_PHYSICAL_SIZE(chunk)))
+
+#define MEMORY_CHUNK_FROM_DATA_ADDRESS(address) ((MEMORY_CHUNK *)((char *)(address) - FENCES - sizeof(MEMORY_CHUNK)))
+
+int heap_setup()
+{
+    memory_manager.first_memory_chunk = NULL;
+    memory_manager.memory_size = 0;
+    memory_manager.memory_start = NULL;
+    return 0;
+}
+
+void heap_clean()
+{
+
+}
+
+void* heap_malloc(size_t size)
+{
+    if (size == 0 || heap_validate())
+    {
+        return NULL;
+    }
+
+    MEMORY_CHUNK *memory_chunk = memory_manager.first_memory_chunk;
+
+    while (memory_chunk)
+    {
+        if (memory_chunk->free == FREED && memory_chunk->size >= size)
+        {
+            memory_chunk->size = size;
+            memory_chunk->free = USED;
+            // TODO: fences
+            return MEMORY_CHUNK_DATA_ADDRESS(memory_chunk);
+        }
+
+        if (memory_chunk->next == NULL)
+        {
+            // TODO: sbrk if not enough space
+            MEMORY_CHUNK *next = MEMORY_CHUNK_NEXT(memory_chunk);
+            memory_chunk->next = next;
+            next->prev = memory_chunk;
+            next->next = NULL;
+            next->size = size;
+            next->free = USED;
+            // TODO: fences
+            return MEMORY_CHUNK_DATA_ADDRESS(next);
+        }
+    }
+
+    memory_manager.memory_start = custom_sbrk(ALIGN_PAGE(size));
+    if (memory_manager.memory_start == SBRK_FAIL)
+    {
+        return NULL;
+    }
+
+    memory_chunk = (MEMORY_CHUNK *)memory_manager.memory_start;
+    memory_chunk->size = size;
+    memory_chunk->free = USED;
+    memory_chunk->prev = NULL;
+    memory_chunk->next = NULL;
+    // TODO: fences
+    return MEMORY_CHUNK_DATA_ADDRESS(memory_chunk);
+}
+
+void* heap_calloc(size_t number, size_t size)
+{
+    (void)number;(void)size;
+    return NULL;
+}
+
+void* heap_realloc(void* address, size_t count)
+{
+    (void)address;(void)count;
+    return NULL;
+}
+
+void heap_free(void* address)
+{
+    (void)address;
+}
+
+size_t heap_get_largest_used_block_size()
+{
+    return 0;
+}
+
+enum pointer_type_t get_pointer_type(const void* ptr)
+{
+    (void)ptr;
+    return pointer_null;
+}
+
+int heap_validate()
+{
+    return 0;
+}
+
+void* heap_malloc_aligned(size_t size)
+{
+    (void)size;
+    return NULL;
+}
+
+void* heap_calloc_aligned(size_t number, size_t size)
+{
+    (void)number;(void)size;
+    return NULL;
+}
+
+void* heap_realloc_aligned(void* address, size_t size)
+{
+    (void)address;(void)size;
+    return NULL;
+}
---
+++ heap.h
@@ -0,0 +1,60 @@
+#ifndef HEAP_H
+#define HEAP_H
+#include <stdint.h>
+#include "custom_unistd.h"
+
+enum mem_flag_t
+{
+    USED = 0,
+    FREED = 1
+};
+
+enum pointer_type_t
+{
+    pointer_null,
+    pointer_heap_corrupted,
+    pointer_control_block,
+    pointer_inside_fences,
+    pointer_inside_data_block,
+    pointer_unallocated,
+    pointer_valid
+};
+
+typedef struct memory_manager_t
+{
+    void *memory_start;
+    size_t memory_size;
+    struct memory_chunk_t *first_memory_chunk;
+} MEMORY_MANAGER;
+
+typedef struct memory_chunk_t
+{
+    struct memory_chunk_t* prev;
+    struct memory_chunk_t* next;
+    size_t size;
+    int free;
+} MEMORY_CHUNK;
+
+int heap_setup();
+void heap_clean();
+
+void* heap_malloc(size_t size);
+void* heap_calloc(size_t number, size_t size);
+void* heap_realloc(void* address, size_t count);
+void heap_free(void* address);
+
+size_t heap_get_largest_used_block_size();
+enum pointer_type_t get_pointer_type(const void* ptr);
+
+int heap_validate();
+
+void* heap_malloc_aligned(size_t size);
+void* heap_calloc_aligned(size_t number, size_t size);
+void* heap_realloc_aligned(void* address, size_t size);
+
+#ifndef CUSTOM_SBRK
+#define CUSTOM_SBRK
+extern void* custom_sbrk(intptr_t delta);
+#endif
+
+#endif //HEAP_H
