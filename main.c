39740cc382e7dfd85263a518322d7d8fe64b3bb0
Damian Mendel <mendeldamian01@gmail.com>
2022-09-14 20:13:41 +0200

    clean main

--- main.c
+++ main.c
@@ -1,22 +1,4 @@
-#include "heap.h"
-
 int main(void)
 {
-    heap_setup();
-
-    char *ptr1 = heap_malloc(1000);
-    heap_realloc(ptr1, 6000);
-//    heap_malloc(1000);
-//    heap_free(ptr1);
-//    ptr1 = heap_malloc(100);
-//    ptr1 = heap_realloc(ptr1, 1000);
-//    heap_realloc(ptr1, 0);
-
-    heap_print();
-    heap_print_chunks();
-
-
-
-    heap_clean();
     return 0;
 }
